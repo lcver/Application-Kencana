ab55d5ffad058c2e21fe386e521cb5f26c8451a4
lcver <crackover1092@gmail.com>
2020-06-05 01:03:06 +0700

    [script] Guru Controller

--- App/controllers/GuruController.php
+++ App/controllers/GuruController.php
@@ -4,8 +4,18 @@ use App\Core\Controller;
 
 class GuruController extends Controller
 {
+    public function __construct()
+    {
+        if(!isset($_SESSION['kencana_usersession']))
+            header('location:'.BASEURL.'auth');
+    }
     public function index()
     {
         $this->view("guru/index",[],"admin");
+    }
+
+    public function bank_soal()
+    {
+        $this->view("guru/bank_soal",[],"admin");
     }
 }
