a620185014f2167bdd70b64aa29572a1ccb30c27
lcver <crackover1092@gmail.com>
2020-06-05 01:03:33 +0700

    [script] Kasek Controller

--- App/controllers/KasekController.php
+++ App/controllers/KasekController.php
@@ -4,8 +4,14 @@ use App\Core\Controller;
 
 class KasekController extends Controller
 {
+    public function __construct()
+    {
+        if(!isset($_SESSION['kencana_usersession']))
+            header('location:'.BASEURL.'auth');
+    }
+    
     public function index()
     {
-        
+        $this->view('kasek/index',[],'admin');
     }
 }
