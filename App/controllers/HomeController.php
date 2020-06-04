933844ab734c1142ce8d9c3e98270c6e725e0bd8
lcver <crackover1092@gmail.com>
2020-06-05 01:03:53 +0700

    [script] Home Controller

--- App/controllers/HomeController.php
+++ App/controllers/HomeController.php
@@ -4,6 +4,12 @@ use App\Core\Controller;
 
 class HomeController extends Controller
 {
+    public function __construct()
+    {
+        if(!isset($_SESSION['kencana_usersession']))
+            header('location:'.BASEURL.'auth');
+    }
+    
     public function index()
     {
         // echo "Home index";
