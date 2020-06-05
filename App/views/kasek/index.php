499c46e138946f3505deb6dac743e32c02522d27
lcver <crackover1092@gmail.com>
2020-06-05 12:11:56 +0700

    [layout] Kasek admin dahboard

---
+++ App/views/kasek/index.php
@@ -0,0 +1,192 @@
+<div class="pt-4">
+    <!-- Default box -->
+    <div class="card">
+    <div class="card-header">
+        <h3 class="card-title">Kinerja Guru</h3>
+
+        <div class="card-tools">
+        <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+            <i class="fas fa-minus"></i></button>
+        <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
+            <i class="fas fa-times"></i></button>
+        </div>
+    </div>
+    <div class="card-body p-0">
+        <table class="table table-striped projects">
+            <thead>
+                <tr>
+                    <th style="width: 1%">
+                        #
+                    </th>
+                    <th style="width: 20%">
+                        Nama Guru
+                    </th>
+                    <th style="width: 30%">
+                        Peran Guru
+                    </th>
+                    <th>
+                        Progress Guru
+                    </th>
+                    <th style="width: 20%">
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        #
+                    </td>
+                    <td>
+                        <a>
+                            Marina Polean
+                        </a>
+                        <br/>
+                        <small>
+                            Matapelajaran
+                        </small>
+                    </td>
+                    <td>
+                        <ul class="list-inline">
+                            <li class="list-inline-item">
+                                Bahasa Inggris
+                            </li>
+                        </ul>
+                    </td>
+                    <td class="project_progress">
+                        <div class="progress progress-sm">
+                            <div class="progress-bar bg-green" role="progressbar" aria-volumenow="100" aria-volumemin="0" aria-volumemax="100" style="width: 100%">
+                            </div>
+                        </div>
+                        <small>
+                            100% Complete
+                        </small>
+                    </td>
+                    <td class="project-actions text-right">
+                        <a class="btn btn-primary btn-sm" href="#">
+                            <i class="fas fa-folder">
+                            </i>
+                            View
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        #
+                    </td>
+                    <td>
+                        <a>
+                            Nana Sution
+                        </a>
+                        <br/>
+                        <small>
+                            Walikelas
+                        </small>
+                    </td>
+                    <td>
+                        <ul class="list-inline">
+                            <li class="list-inline-item">
+                                1A
+                            </li>
+                        </ul>
+                    </td>
+                    <td class="project_progress">
+                        <div class="progress progress-sm">
+                            <div class="progress-bar bg-green" role="progressbar" aria-volumenow="47" aria-volumemin="0" aria-volumemax="100" style="width: 47%">
+                            </div>
+                        </div>
+                        <small>
+                            47% Complete
+                        </small>
+                    </td>
+                    <td class="project-actions text-right">
+                        <a class="btn btn-primary btn-sm" href="#">
+                            <i class="fas fa-folder">
+                            </i>
+                            View
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        #
+                    </td>
+                    <td>
+                        <a>
+                            Azka sudirman
+                        </a>
+                        <br/>
+                        <small>
+                            Matapelajaran
+                        </small>
+                    </td>
+                    <td>
+                        <ul class="list-inline">
+                            <li class="list-inline-item">
+                                Teknik Ilmu Komputer
+                            </li>
+                        </ul>
+                    </td>
+                    <td class="project_progress">
+                        <div class="progress progress-sm">
+                            <div class="progress-bar bg-green" role="progressbar" aria-volumenow="77" aria-volumemin="0" aria-volumemax="100" style="width: 77%">
+                            </div>
+                        </div>
+                        <small>
+                            77% Complete
+                        </small>
+                    </td>
+                    <td class="project-actions text-right">
+                        <a class="btn btn-primary btn-sm" href="#">
+                            <i class="fas fa-folder">
+                            </i>
+                            View
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        #
+                    </td>
+                    <td>
+                        <a>
+                            Diana Dwa Putra
+                        </a>
+                        <br/>
+                        <small>
+                            Walikelas
+                        </small>
+                    </td>
+                    <td>
+                        <ul class="list-inline">
+                            <li class="list-inline-item">
+                                6A
+                            </li>
+                            <li class="list-inline-item">
+                                6B
+                            </li>
+                        </ul>
+                    </td>
+                    <td class="project_progress">
+                        <div class="progress progress-sm">
+                            <div class="progress-bar bg-green" role="progressbar" aria-volumenow="60" aria-volumemin="0" aria-volumemax="100" style="width: 60%">
+                            </div>
+                        </div>
+                        <small>
+                            60% Complete
+                        </small>
+                    </td>
+                    <td class="project-actions text-right">
+                        <a class="btn btn-primary btn-sm" href="#">
+                            <i class="fas fa-folder">
+                            </i>
+                            View
+                        </a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <!-- /.card-body -->
+    </div>
+    <!-- /.card -->
+</div>
